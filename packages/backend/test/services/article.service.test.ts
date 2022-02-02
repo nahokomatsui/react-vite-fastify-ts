154138703888e50b5e61b42818382efe19765a4c
Nahoko Matsui <nahokomatsui8213@gmail.com>
2022-02-02 23:52:06 +0900

    setup backend unit test with jest

---
+++ packages/backend/test/services/article.service.test.ts
@@ -0,0 +1,20 @@
+import { prismaMock } from "../mocks/prisma";
+import { createArticle } from "../../services/article.service";
+
+describe("articleService", () => {
+  describe("createArticle", () => {
+    test("successfully creates article", async () => {
+      const injectedService = createArticle.inject({
+        prisma: prismaMock,
+      });
+
+      await expect(
+        injectedService({
+          title: "title",
+          tags: ["hoge"],
+        })
+      ).resolves.not.toThrow();
+      expect(prismaMock.article.create).toBeCalledTimes(1);
+    });
+  });
+});
---
+++ packages/backend/test/services/ping.service.test.ts
@@ -0,0 +1,9 @@
+import { pong } from "../../services/ping.service";
+
+describe("pingService", () => {
+  describe("pong", () => {
+    test("succeeds", () => {
+      expect(pong()).toBe("pong!");
+    });
+  });
+});
